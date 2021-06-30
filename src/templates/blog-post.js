8f368b9fbb229ebb09e6ccc735cd4057ee33d1af
Jisang Hong <wona23@gmail.com>
2021-06-30 16:36:37 +0900

    Post 쿼리의 thumbnail 필드 제거

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -28,18 +28,11 @@ export default ({ data, pageContext, location }) => {
   const metaData = data.site.siteMetadata
   const { title, comment, siteUrl, author, sponsor } = metaData
   const { utterances } = comment
-  const { title: postTitle, date, thumbnail } = post.frontmatter
-  const thumbnailSrc = thumbnail
-    ? `${siteUrl}${thumbnail.childImageSharp.fixed.src}`
-    : undefined
+  const { title: postTitle, date } = post.frontmatter
 
   return (
     <Layout location={location} title={title}>
-      <Head
-        title={postTitle}
-        description={post.excerpt}
-        thumbnail={thumbnailSrc}
-      />
+      <Head title={postTitle} description={post.excerpt} />
       <PostTitle title={postTitle} />
       <PostDate date={date} />
       <PostContainer html={post.html} />
@@ -78,13 +71,6 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
-        # thumbnail {
-        #   childImageSharp {
-        #     fixed(width: 800) {
-        #       src
-        #     }
-        #   }
-        # }
       }
     }
   }
